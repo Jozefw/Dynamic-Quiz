2d99e33e31567869204fefa697eee971bd567dd6
Jozef <jozefw@sbcglobal.net>
2015-04-30 13:33:08 -0700

    second commit

---
+++ main.js
@@ -0,0 +1,16 @@
+(function(){
+	var questionCounter = 0,
+		selections=[],
+		quiz=$('#quiz'),
+		questions =[{
+			question: "what is 1+1",
+			choices:[1,2,3],
+			answer: 2,
+		}, {
+			question: "what color is orange",
+			choices: [blue, porange, orange],
+			answer: orange,
+		}];
+
+
+})();
